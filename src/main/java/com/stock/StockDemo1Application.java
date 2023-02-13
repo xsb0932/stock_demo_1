65a2301a83a60c47c3b4fca7c2d62f6abe43f8ef
XuShiBai <xsb_terry@163.com>
2023-02-13 16:52:58 +0800

    frist commit

---
+++ src/main/java/com/stock/StockDemo1Application.java
@@ -0,0 +1,13 @@
+package com.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockDemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/stock/StockDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
